02d3526a9ca46cea3592b2f96e864fcc2ff90719
Irene Díez <diez.irene@opendeusto.es>
2014-03-24 19:50:08 +0100

    A* algorithm added

---
+++ BlackWhiteSquares/src/algorithms/AStar.java
@@ -0,0 +1,79 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import es.deusto.ingenieria.is.search.algorithms.Node;
+import es.deusto.ingenieria.is.search.algorithms.heuristic.EvaluationFunction;
+import es.deusto.ingenieria.is.search.algorithms.heuristic.HeuristicSearchMethod;
+import es.deusto.ingenieria.is.search.formulation.Operator;
+import es.deusto.ingenieria.is.search.formulation.Problem;
+import es.deusto.ingenieria.is.search.formulation.State;
+
+public class AStar extends HeuristicSearchMethod {
+
+	public AStar(EvaluationFunction function) {
+		super(function);
+	}
+	
+	public Node search(Problem problem, State initialState) { // TODO not tested
+		// Make a node with the initial problem state
+		Node firstNode= new Node(initialState);
+		// Insert node into the frontier data structure
+		List<Node> frontier= new ArrayList<Node>();
+		frontier.add(firstNode);
+		// (Variables needed later on)
+		List<Node> successorNodes= null;
+		boolean solutionFound= false;
+		// WHILE final state not found AND frontier is NOT empty DO
+		while(!solutionFound && !frontier.isEmpty()){
+			// Remove first node from the frontier
+			firstNode= frontier.remove(0);
+			// IF node contains final state
+			if(problem.isFinalState(firstNode.getState()))
+				// THEN final state found
+				solutionFound= true;
+			// IF node does not contain final state
+			else{
+				// EXPAND node's state
+				successorNodes= expand(firstNode, problem);
+				// Insert successor nodes into the frontier
+				frontier.addAll(successorNodes);
+				// Sort frontier in ascending order of f(n)
+				Collections.sort(frontier);
+			}
+		}
+		// IF final state found THEN return sequence of actions found
+		// ELSE return "solution not found"
+		return solutionFound ? firstNode : null;
+	}
+	
+	protected List<Node> expand(Node node, Problem problem) { // TODO not tested
+		// Initialize Successors to an empty list
+		List<Node> successorNodes= new ArrayList<Node>();
+		// FOR EACH applicable action on state of current node DO
+		for(Operator operator : problem.getOperators()){
+			// Make a new successor node and generate succesor state
+			State succesorState= operator.apply(node.getState());
+			// (If the operator was applicable then a new state was generated and so succesorState != null)
+			if(succesorState != null){
+				// Successor node state = succesor state
+				Node succesorNode= new Node(succesorState);
+				// Successor node action = applicable action
+				succesorNode.setOperator(operator.getName());
+				// Successor node depth = current node depth + 1
+				succesorNode.setDepth(node.getDepth() + 1);
+				// Compute f(succ) =  g(succ) + h(succ)
+				// TODO review
+				succesorNode.setH(this.getEvaluationFunction().calculateG(succesorNode) + this.getEvaluationFunction().calculateH(succesorNode));
+				// (Other things)
+				succesorNode.setParent(node);
+				// Add successor node to Successors
+				successorNodes.add(succesorNode);
+			}
+		}
+		// return Successors
+		return successorNodes;
+	}
+}
--- README.md
+++ README.md
@@ -5,12 +5,10 @@ Intelligent Systems project
 
 ##To do list:
 
-* ~~To define/redefine clone() in each object that makes up the problem environment. Make sure you test the clone() method of each environment object before you invoke clone() from elsewhere in your program.~~
-* ~~To update the method effect() of your Operator subclasses in such a way that effect() makes use of clone().~~
-* ~~To define a method solve(Search algorithm) within the BlackWhiteSquares class that solves the whole problem and fix bugs. This is your first complete system version.~~
-* ~~To answer question 3 of the General Problem Case Description.~~
-* ~~To analyse the performance of your system with Blind Search Methods DEPTH FIRST SEARCH and BREADTH FIRST SEARCH. You will do this for the problem instance described by the .xml file accessible in ALUD2. In accordance with the learning goals specified above, for each Blind method you are required to:~~
-	* ~~Analyse how different orderings of Operator instances in the list of Operators of your BlackWhiteSquaresProblem instance affect the solution found by the search algorithm. Explain your findings.~~
-	* ~~Discuss the quality of the solution provided by the system and determine whether it may be improved and if so, how.~~
-	* ~~Time required by the search algorithm to produce a solution and discuss whether it may be improved without compromising the quality of the solution (or even improving it?) and if so, how.~~
+* To answer questions 4 and 5 of the General Problem Case Description
+* To code the heuristic you have just defined as a subclass of the EvaluationFunction class.
+* To use this heuristic with Best First Search and compare the quality of the solution provided by Best First Search against that obtained with Depth First Search and also the one obtained with Breadth First Search. In order to make this comparison try 2 or 3 different orderings of your operators. One of the orderings must be: Move-2Steps, Move-4Steps, Move-1Step.
+* ~~To implement A* Search as a subclass of the HeuristicSearchMethod class. You must implement the simple version of the algorithm, Basic (or Tree) Search as we called it in class, that DOESN’T CHECK FOR REPEATED STATES. Use the pseudo-code in your slides as reference.
+* Use your A* Search algorithm with an evaluation function that uses the heuristic you have previously defined. Compare the solution provided by your algorithm against that provided by each of the previous algorithms: Best First Search, Depth First Search and Breadth First Search. Try the operator ordering: Move-2Steps, Move-4Steps, Move-1Step.
+* Think about the State Space of the problem you are addressing and answer: If you implemented Best First Search in its simple version (Basic Search) NOT CHECKING FOR REPEATED STATES (alike A* above), is it possible for some instance of the problem that this simple version of Best First Search got stuck within a loop unable to provide a solution? And how about your implementation of A*? Justify your answers.
 
